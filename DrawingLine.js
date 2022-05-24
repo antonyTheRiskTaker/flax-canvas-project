fa273f8ee6c3ab15c46dfe42f711dbe243dedf05
Antony Lo <mrmikadox@gmail.com>
2022-05-24 14:59:06 +0800

    add DrawingLine.js class

---
+++ DrawingLine.js
@@ -0,0 +1,34 @@
+class DrawingLine extends PaintFunction {
+  constructor(ctxReal) {
+    super();
+    this.ctx = ctxReal;
+  }
+
+  onMouseDown(coord, e) {
+    this.ctx.strokeStyle = '#df4b26';
+    this.ctx.lineJoin = 'round';
+    this.ctx.lineWidth = 5;
+    this.ctx.beginPath();
+    this.ctx.moveTo(coord[0], coord[1]);
+    this.draw(coord[0], coord[1]);
+  }
+
+  onDragging(coord, e) {
+    this.draw(coord[0], coord[1]);
+  }
+
+  onMouseMove() {} // Assume it's not needed
+
+  onMouseUp() {} // Assume it's not needed
+
+  onMouseLeave() {} // Assume it's not needed
+
+  onMouseEnter() {} // Assume it's not needed
+
+  draw(x, y) {
+    this.ctx.lineTo(x, y);
+    this.ctx.moveTo(x, y);
+    this.ctx.closePath();
+    this.ctx.stroke();
+  }
+}
