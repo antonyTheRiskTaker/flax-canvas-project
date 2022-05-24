23dfcf702c946b7cba1d365532aad86199abb626
Antony Lo <mrmikadox@gmail.com>
2022-05-24 14:14:22 +0800

    add DrawingRectangle class

---
+++ DrawingRectangle.js
@@ -0,0 +1,30 @@
+class DrawingRectangle extends PaintFunction {
+  constructor(ctxReal, ctxDraft) {
+    super();
+    this.ctxReal = ctxReal;
+    this.ctxDraft = ctxDraft;
+  }
+
+  onMouseDown(coord, e) {
+    this.ctxReal.fillStyle = '#f44';
+    this.origX = coord[0];
+    this.origY = coord[1];
+  }
+
+  onDragging(coord, e) {
+    this.ctxDraft.fillStyle = '#f44';
+    this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+    this.ctxDraft.fillRect(this.origX, this.origY, coord[0] - this.origX, coord[1] - this.origY);
+  }
+
+  onMouseMove() {} // ?
+
+  onMouseUp(coord) {
+    this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvas.height);
+    this.ctxReadl.fillRect(this.origX, this.origY, coord[0] - this.origX,  coord[1] - this.origY);
+  }
+
+  onMouseLeave() {} // ?
+
+  onMouseEnter() {} // ? 
+}
