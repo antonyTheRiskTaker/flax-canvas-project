040a6b3aa482d817a8f8ba7a7935f4256e952a98
Antony Lo <mrmikadox@gmail.com>
2022-05-26 17:05:04 +0800

    add jquery code to make sure the page is loaded before mouse events are heard

--- canvas-common.js
+++ canvas-common.js
@@ -7,49 +7,52 @@ let dragging = false;
 
 // I wonder if I should add a $(() => {}) to encompass all these event handlers;
 // $(() => {}); is equivalent to $(document).ready(() => {}); which is deprecated
+$(() => {
 
-$('#canvas-draft').on('mousedown', e => {
-  const mouseX = e.offsetX;
-  const mouseY = e.offsetY;
-  currentFunction.onMouseDown([mouseX, mouseY], e);
-  dragging = true;
-});
+  $('#canvas-draft').on('mousedown', e => {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    currentFunction.onMouseDown([mouseX, mouseY], e);
+    dragging = true;
+  });
 
-$('#canvas-draft').on('mousemove', e => {
-  const mouseX = e.offsetX;
-  const mouseY = e.offsetY;
-  if (dragging) {
-    currentFunction.onDragging([mouseX, mouseY], e);
-  }
-  currentFunction.onMouseMove([mouseX, mouseY], e);
-});
+  $('#canvas-draft').on('mousemove', e => {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    if (dragging) {
+      currentFunction.onDragging([mouseX, mouseY], e);
+    }
+    currentFunction.onMouseMove([mouseX, mouseY], e);
+  });
 
-$('#canvas-draft').on('mouseup', e => {
-  dragging = false;
-  const mouseX = e.offsetX;
-  const mouseY = e.offsetY;
-  currentFunction.onMouseUp([mouseX, mouseY], e);
-});
+  $('#canvas-draft').on('mouseup', e => {
+    dragging = false;
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    currentFunction.onMouseUp([mouseX, mouseY], e);
+  });
 
-$('#canvas-draft').on('mouseleave', e => {
-  dragging = false;
-  const mouseX = e.offsetX;
-  const mouseY = e.offsetY;
-  currentFunction.onMouseLeave([mouseX, mouseY], e);
-});
+  $('#canvas-draft').on('mouseleave', e => {
+    dragging = false;
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    currentFunction.onMouseLeave([mouseX, mouseY], e);
+  });
 
-$('#canvas-draft').on('mouseenter', e => {
-  const mouseX = e.offsetX;
-  const mouseY = e.offsetY;
-  currentFunction.onMouseEnter([mouseX, mouseY], e);
+  $('#canvas-draft').on('mouseenter', e => {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
+    currentFunction.onMouseEnter([mouseX, mouseY], e);
+  });
+  
 });
 
 class PaintFunction {
-  constructor() {}
-  onMouseDown() {}
-  onDragging() {}
-  onMouseMove() {}
-  onMouseUp() {}
-  onMouseLeave() {}
-  onMouseEnter() {}
+  constructor() { }
+  onMouseDown() { }
+  onDragging() { }
+  onMouseMove() { }
+  onMouseUp() { }
+  onMouseLeave() { }
+  onMouseEnter() { }
 }
