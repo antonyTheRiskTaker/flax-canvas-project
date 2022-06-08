01b14c70b69f45619cde23034cb69ba0076043c1
Antony Lo <mrmikadox@gmail.com>
2022-06-08 15:03:22 +0800

    drawing curve function kinda works but needs to fix some issues

--- DrawingCurvedLine.js
+++ DrawingCurvedLine.js
@@ -4,33 +4,48 @@ class DrawingCurvedLine extends PaintFunction {
     this.ctxReal = ctxReal;
     this.ctxDraft = ctxDraft;
     this.drawColour = drawColour;
+    this.clicked = 0;
   }
 
   onMouseDown(coord, e) {
+    console.log('Mousedown');
     this.ctxReal.strokeStyle = this.drawColour;
     this.ctxReal.lineJoin = 'round';
     this.ctxReal.lineWidth = 5;
-    this.origX = coord[0];
-    this.origY = coord[1];
+    
+    if (this.clicked === 0) {
+      this.origX = coord[0];
+      this.origY = coord[1];
+      this.clicked++;
+    } else if (this.clicked === 1) {
+      this.endX = coord[0];
+      this.endY = coord[1];
+      this.clicked++;
+    }
   }
 
   onDragging(coord, e) {
+    console.log('Dragging');
+    this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
     this.ctxDraft.strokeStyle = this.drawColour;
     this.ctxDraft.lineJoin = 'round';
     this.ctxDraft.lineWidth = 5;
-    this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
     this.ctxDraft.moveTo(this.origX, this.origY);
-    this.ctxDraft.quadraticCurveTo(this.origX*1.5, this.origY*1.5, this.origX, this.origY*5); // Fill in the arguments
+    this.ctxDraft.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
     this.ctxDraft.stroke();
   }
 
   onMouseMove() {}
 
   onMouseUp(coord, e) {
-    this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-    this.ctxReal.moveTo(this.origX, this.origY);
-    this.ctxReal.quadraticCurveTo(this.origX*1.5, this.origY*1.5, this.origX, this.origY*5); // Fill in the arguments
-    this.ctxReal.stroke();
+    if (this.clicked === 2) {
+      // reset coord
+      this.clicked = 0;
+      this.ctxDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+      this.ctxReal.moveTo(this.origX, this.origY);
+      this.ctxReal.quadraticCurveTo(coord[0], coord[1], this.endX, this.endY);
+      this.ctxReal.stroke();
+    }
   }
 
   onMouseLeave() {}
